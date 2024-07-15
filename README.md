41809d0e282797f9163fefb521ac8d595faa3e3f
Daniel C <dacruva@gmail.com>
2024-07-15 15:49:59 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<h1 align="center">Welcome to workout-tracking 👋</h1>
+<p>
+</p>
+
+> This project integrates Natural Language  provided by https://nutritionix.com and the result is saved in a google sheet using https://sheety.co/ APIS to connect to google.  
+
+## Usage
+
+```sh
+python main.py
+```
+
+## Author
+
+👤 **Daniel Varilla**
+
+* Github: [@dacruva](https://github.com/dacruva)
+* LinkedIn: [@danielvarilla](https://linkedin.com/in/danielvarilla)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
---
+++ main.py
@@ -0,0 +1,67 @@
+import os
+from dotenv import load_dotenv
+import requests
+from datetime import datetime
+
+load_dotenv()
+
+END_POINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+app_id = os.getenv("APP_ID")
+app_key = os.getenv("APP_KEY")
+sheety_endpoint = os.getenv("SHEETY_ENDPOINT")
+sheety_user = os.getenv("SHEETY_USER")
+sheety_pass = os.getenv("SHEETY_PASS")
+
+
+# data used to calculate calories
+GENDER = "male"
+WEIGHT_KG = 84
+HEIGHT_CM = 180
+AGE = 32
+
+headers = {
+    "x-app-id": app_id,
+    "x-app-key": app_key,
+}
+
+exercise_text = input("Tell me which exercises you did: ")
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+request = requests.post(url=END_POINT, json=parameters, headers=headers)
+response = request.json()
+print(f"Call to Nutritionix API: \n {response} \n")
+
+# Creating variables for date and time
+today_date = datetime.now().strftime("%d/%m/%y")
+now_time = datetime.now().strftime("%X")
+SHEET_NAME = "workout"
+sheet_headers = {
+    "Authorization": f"Bearer {os.getenv('SHEETY_BEARER')}"
+}
+
+for exercise in response["exercises"]:
+    sheet_inputs = {
+        SHEET_NAME: {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+    print(sheet_inputs)
+
+    sheet_request = requests.post(
+        url=sheety_endpoint, json=sheet_inputs, headers=sheet_headers,
+    )
+    print(sheet_request.status_code)
+    print(sheet_request.json())
+
+
+
